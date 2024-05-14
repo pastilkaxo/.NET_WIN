97417be22a8f656348b3de7ad9082359dd42ee6e
Vlad <vlad.lemeshok@gmail.com>
2024-05-15 02:27:55 +0300

    fix

---
+++ OOP9_10/ООП9/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП9
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private BankContext _bc;
+        private DbSet<T> _entity;
+
+        public Repository(BankContext bc)
+        {
+            _bc = bc;
+            _entity = bc.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _entity.Add(entity);
+            _bc.SaveChanges();
+
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _entity.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _entity.Find(id);
+        }
+
+        public void Remove(T entity)
+        {
+            _entity.Remove(entity);
+            _bc.SaveChanges();
+
+        }
+
+        public void Update(T entity)
+        {
+            _entity.AddOrUpdate(entity);
+            _bc.SaveChanges();
+        }
+    }
+}
---
+++ OOP9_10/ООП9/UnitOfWork.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП9
+{
+    public class UnitOfWork : IDisposable
+    {
+        private BankContext bankContext = new BankContext();
+        private Repository<Owner> ownerRepos;
+        private Repository<ClientCheck> checkRepos;
+        private bool disposed = false;
+
+        public Repository<Owner> Owners
+        {
+            get
+            {
+                if(ownerRepos == null) {
+                    ownerRepos = new Repository<Owner>(bankContext);
+                }
+                return ownerRepos;
+            }
+        }
+
+        public Repository<ClientCheck> ClientChecks
+        {
+            get
+            {
+                if (checkRepos == null)
+                {
+                    checkRepos = new Repository<ClientCheck>(bankContext);  
+                }
+                return checkRepos;
+            }
+        }
+
+        public void Save()
+        {
+            bankContext.SaveChanges();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if(!this.disposed)
+            {
+                if(disposing)
+                {
+                    bankContext.Dispose();  
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);  
+            GC.SuppressFinalize(this);
+        }
+    }
+}
